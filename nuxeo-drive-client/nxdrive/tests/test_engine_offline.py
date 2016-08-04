05f07a54395b5e5ceb73bbc99f1f4074c687e392
Gopalakrishnan <tgopalakrsihnan@ssdi.sharp.co.in>
2016-08-04 20:33:14 +0530

    CSPII-10276: Reduced sleep time to mininmize test case execution time

--- nuxeo-drive-client/nxdrive/tests/test_engine_offline.py
+++ nuxeo-drive-client/nxdrive/tests/test_engine_offline.py
@@ -8,12 +8,13 @@ Files/folders should not sync while engine is paused due to a network error. In 
 '''
 
 from common_unit_test import UnitTestCase
+# To re-use time delay between two successive GetChangeSummary calls
+from common import TEST_DEFAULT_DELAY
 import nxdrive
 from mock import patch
 from urllib2 import URLError
 from time import sleep
 from nxdrive.client.base_automation_client import BaseAutomationClient
-
 
 
 class EngineOffLineTestCase(UnitTestCase):
@@ -32,7 +33,7 @@ class EngineOffLineTestCase(UnitTestCase):
         super(EngineOffLineTestCase, self).setUp()
         # Start engine and wait for Sync
         self.engine_1.start()
-        self.wait_sync(wait_for_async=30)
+        self.wait_sync(wait_for_async=True, timeout=30)
 
     def tearDown(self):
         pass
@@ -49,6 +50,19 @@ class EngineOffLineTestCase(UnitTestCase):
             raise URLError("server is offline in mock_execute:")
         return EngineOffLineTestCase.original_execute(self, *args, **kwargs)
 
+    def wait_for_upload(self, items=[], timeout=30):
+        '''
+            Wait for a list of file/folders to upload
+        '''
+        elapsed = 0
+        not_synced = items
+        while not_synced and elapsed < timeout:
+            sleep(2)
+            elapsed = elapsed + 2
+            states = [self.get_dao_state_from_engine_1(item) for item in items]
+            not_synced = [state for state in states if state and str(state.pair_state) != 'synchronized']
+        return states
+
     @patch.object(nxdrive.client.base_automation_client.BaseAutomationClient, 'fetch_api', mock_fetch_api)
     @patch.object(nxdrive.client.base_automation_client.BaseAutomationClient, 'execute', mock_execute)
     def test_with_mock_server_offline(self):
@@ -64,13 +78,13 @@ class EngineOffLineTestCase(UnitTestCase):
 
         EngineOffLineTestCase.server_online = False
         # Wait for GetChangeSummary to call check_offline method
-        sleep(35)
+        sleep(TEST_DEFAULT_DELAY + 2)
 
         # Create a folder inside user folder and a file inside the folder
         self.local_client_1.make_folder('/', 'FolderA')
         self.local_client_1.make_file('/FolderA', 'TestFile.txt', content="test network failure")
         # Wait for events to be handled
-        sleep(35)
+        sleep(TEST_DEFAULT_DELAY + 2)
 
         # Check pair_state and error_count for the folder/file because it should not try to sync when engine is offline
         test_folder = self.get_dao_state_from_engine_1('/FolderA')
@@ -82,17 +96,13 @@ class EngineOffLineTestCase(UnitTestCase):
 
         # Stop mocking and call original method
         EngineOffLineTestCase.server_online = True
-        # Wait for queue_manager to process for the folder/file
-        sleep(60)
 
-        # pair_state should be 'synchronized' for the folder/file
-        test_folder = self.get_dao_state_from_engine_1('/FolderA')
-        self.assertEqual(test_folder.pair_state, 'synchronized')
-        test_file = self.get_dao_state_from_engine_1('/FolderA/TestFile.txt')
-        self.assertEqual(test_file.pair_state, 'synchronized')
+        # Wait for files to upload
+        states = self.wait_for_upload(items=['/FolderA', '/FolderA/TestFile.txt'])
+        for state in states:
+            self.assertEqual(state.pair_state, 'synchronized')
 
     def test_with_manual_pause_resume(self):
-
         '''
         1. Pause the engine
         2. Create a local folder inside user folder
@@ -101,7 +111,6 @@ class EngineOffLineTestCase(UnitTestCase):
         5. Resume the engine
         4. Check the pair_state when engine is resumed
         '''
-
         # Suspend the engine
         self.engine_1.suspend()
 
@@ -109,7 +118,7 @@ class EngineOffLineTestCase(UnitTestCase):
         self.local_client_1.make_folder('/', 'FolderB')
         self.local_client_1.make_file('/FolderB', 'TestFile1.txt', content="test manual pause and resume")
         # Wait for events to be handled
-        sleep(35)
+        sleep(TEST_DEFAULT_DELAY + 2)
 
         # queue manager will not process for the folder/file when engine is paused and
         # database should not have entries for the folder/file
@@ -120,11 +129,8 @@ class EngineOffLineTestCase(UnitTestCase):
 
         # Resume the engine
         self.engine_1.resume()
-        # Wait for queue_manager to process the folder/file
-        sleep(25) # To avoid failure, if any delay in server response
 
-        # pair_state should be 'synchronized' for the folder/file
-        test_folder = self.get_dao_state_from_engine_1('/FolderB')
-        self.assertEqual(test_folder.pair_state, 'synchronized')
-        test_file = self.get_dao_state_from_engine_1('/FolderB/TestFile1.txt')
-        self.assertEqual(test_file.pair_state, 'synchronized')
+        # Wait for files to upload
+        states = self.wait_for_upload(items=['/FolderB', '/FolderB/TestFile1.txt'])
+        for state in states:
+            self.assertEqual(state.pair_state, 'synchronized')
